2670d3337418183a82a53dd047427234d3c87671
Jxxunnn <roof2586@naver.com>
2023-03-18 16:20:14 +0900

    study react-query

---
+++ react/state-management/react-query/src/App copy.tsx
@@ -0,0 +1,53 @@
+import { useState } from "react";
+import { useQuery, QueryClient, QueryClientProvider } from "react-query";
+import { isRouteErrorResponse } from "react-router-dom";
+import "./App.css";
+
+function App() {
+  const queryClient = new QueryClient();
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Users />
+    </QueryClientProvider>
+  );
+}
+
+interface UsersProps {
+  id: number;
+  name: string;
+  [key: string]: string | number;
+}
+
+function Users() {
+  const fetchUsers = async () => {
+    const url = "https://jsonplaceholder.typicode.com/users";
+    const res = await fetch(url);
+    return res.json();
+  };
+
+  const { isLoading, isError, data, error } = useQuery<UsersProps[]>(
+    "users",
+    fetchUsers
+  );
+
+  if (isLoading) {
+    return <span>Loading...</span>;
+  }
+  if (isError) {
+    let message = "Unknown Error";
+    if (error instanceof Error) message = error.message;
+
+    return <span>Error: {message}</span>;
+  }
+
+  return (
+    <ul>
+      {data?.map((d) => (
+        <li key={d.id}>{d.name}</li>
+      ))}
+    </ul>
+  );
+}
+
+export default App;
--- react/state-management/react-query/src/App.tsx
+++ react/state-management/react-query/src/App.tsx
@@ -1,53 +1,49 @@
-import { useState } from "react";
-import { useQuery, QueryClient, QueryClientProvider } from "react-query";
-import { isRouteErrorResponse } from "react-router-dom";
-import "./App.css";
+import React from "react";
+import { QueryClient, QueryClientProvider } from "react-query";
 
-function App() {
-  const queryClient = new QueryClient();
+export default function App() {
+  const [page, setPage] = React.useState(0);
 
-  return (
-    <QueryClientProvider client={queryClient}>
-      <Users />
-    </QueryClientProvider>
-  );
-}
+  const fetchProjects = (page = 0) =>
+    fetch("/api/projects?page=" + page).then((res) => res.json());
 
-interface UsersProps {
-  id: number;
-  name: string;
-  [key: string]: string | number;
-}
-
-function Users() {
-  const fetchUsers = async () => {
-    const url = "https://jsonplaceholder.typicode.com/users";
-    const res = await fetch(url);
-    return res.json();
-  };
-
-  const { isLoading, isError, data, error } = useQuery<UsersProps[]>(
-    "users",
-    fetchUsers
-  );
-
-  if (isLoading) {
-    return <span>Loading...</span>;
-  }
-  if (isError) {
-    let message = "Unknown Error";
-    if (error instanceof Error) message = error.message;
-
-    return <span>Error: {message}</span>;
-  }
+  const { isLoading, isError, error, data, isFetching, isPreviousData } =
+    useQuery(["projects", page], () => fetchProjects(page), {
+      keepPreviousData: true,
+    });
 
   return (
-    <ul>
-      {data?.map((d) => (
-        <li key={d.id}>{d.name}</li>
-      ))}
-    </ul>
+    <div>
+      {isLoading ? (
+        <div>Loading...</div>
+      ) : isError ? (
+        <div>Error: {error.message}</div>
+      ) : (
+        <div>
+          {data.projects.map((project) => (
+            <p key={project.id}>{project.name}</p>
+          ))}
+        </div>
+      )}
+      <span>Current Page: {page + 1}</span>
+      <button
+        onClick={() => setPage((old) => Math.max(old - 1, 0))}
+        disabled={page === 0}
+      >
+        Previous Page
+      </button>{" "}
+      <button
+        onClick={() => {
+          if (!isPreviousData && data.hasMore) {
+            setPage((old) => old + 1);
+          }
+        }}
+        // Disable the Next Page button until we know a next page is available
+        disabled={isPreviousData || !data?.hasMore}
+      >
+        Next Page
+      </button>
+      {isFetching ? <span> Loading...</span> : null}{" "}
+    </div>
   );
 }
-
-export default App;
